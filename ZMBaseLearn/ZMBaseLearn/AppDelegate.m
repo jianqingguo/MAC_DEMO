48fa2e37e759a5354fd10e049ff6ff6291f3b5eb
jianqingguo <guojianqing123@gmail.com>
2018-03-07 00:09:20 +0800

    add sort file name button

--- ZMBaseLearn/ZMBaseLearn/AppDelegate.m
+++ ZMBaseLearn/ZMBaseLearn/AppDelegate.m
@@ -24,5 +24,10 @@
   // Insert code here to tear down your application
 }
 
+//- (void)onSortFileNames:()
+- (IBAction)onSortFileNames:(id)sender {
+  NSLog(@"gjq onSortFileNames");
+}
+
 
 @end
